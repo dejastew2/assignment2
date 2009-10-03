64f6de1f528edd5b7c588559262544946942b02e
Isaac <iasay@calpoly.edu>
2009-10-02 22:59:30 -0700

    Added ability to change n value

--- filereadtest.c
+++ filereadtest.c
@@ -2,10 +2,15 @@
 #include <string.h>
 #include <stdlib.h>
 
-
 int main(int argc, char *argv[]) {
 
+	int reset_n_value;
+	int n_value;
 	int i;
+	
+	reset_n_value = 0;
+	n_value = 10;
+	
 	for (i = 1; i < argc; i ++) {
 	
 		char* file_name;
@@ -15,15 +20,26 @@ int main(int argc, char *argv[]) {
 		
 		if ((i == 1) && (file_name[0] == '-')) {
 			if (file_name[1] == 'n') {
-				printf("-n parameter given!\n");
+				reset_n_value = 1;
 			} else {
 				printf("Invalid parameter (-n expected)\n");
+				exit(1);
+			}
+		} else if((i == 2) && (reset_n_value == 1)) {
+			int new_n;
+			new_n = atoi(file_name);
+			if (new_n > 0) {
+				n_value = new_n;
+			} else {
+				printf("Error reading -n option. Must be a positive int!\n");
 				exit(1);
 			}
 		} else {
 			printf("Argument %d: %s\n", i, argv[i]);
 		}
 	}
+	
+	printf("N value is currently: %d\n", n_value);
 	
 	return 0;
 }
