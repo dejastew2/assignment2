c4e5545169ffe83db9222ae5839c87eef7b476d4
Isaac <iasay@calpoly.edu>
2009-10-03 18:44:17 -0700

    Added file read capabilities

--- filereadtest.c
+++ filereadtest.c
@@ -2,22 +2,25 @@
 #include <string.h>
 #include <stdlib.h>
 
+void *safe_malloc(size_t size);
+
 int main(int argc, char *argv[]) {
 
 	int reset_n_value;
 	int n_value;
 	int i;
-	
+	char c;
+
 	reset_n_value = 0;
 	n_value = 10;
-	
+
 	for (i = 1; i < argc; i ++) {
-	
+
 		char* file_name;
-		
-		file_name = (char*)malloc(strlen(argv[i] + 1));
+
+		file_name = (char*)safe_malloc(strlen(argv[i] + 1));
 		strcpy(file_name, argv[i]);
-		
+
 		if ((i == 1) && (file_name[0] == '-')) {
 			if (file_name[1] == 'n') {
 				reset_n_value = 1;
@@ -35,13 +38,32 @@ int main(int argc, char *argv[]) {
 				exit(1);
 			}
 		} else {
-			printf("Argument %d: %s\n", i, argv[i]);
+			FILE *file_pointer;
+			file_pointer = fopen(file_name, "r");
+
+			if (file_pointer != NULL) {
+				while((c = getc(file_pointer)) != EOF) {
+					putchar(c);
+				}
+				fclose(file_pointer);
+			}
+
 		}
-		
+
 		free(file_name);
 	}
-	
-	printf("N value is currently: %d\n", n_value);
-	
+
+	printf("N value is now: %d\n", n_value);
+
 	return 0;
+}
+
+void *safe_malloc(size_t size) {
+	void* ret;
+	ret = malloc(size);
+	if (ret != NULL) {
+		return ret;
+	} else {
+		perror("safe_malloc");
+	}
 }
